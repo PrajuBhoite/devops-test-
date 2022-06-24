742176b7306beabc98856ff656dce1cb3e867bb5
Prajakta Bhoite <p.bhoite@zensar.com>
2022-06-24 17:38:34 +0530

    devops

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+// //import { Greet } from "./Greet";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+// const App = (properties) => {
+//   console.log(properties);
+//   const fileName = "src/App.js";
+//   const Hey = (<h2>Hey, there!</h2>)
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//           Edit <code>{fileName}</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//           {properties.appName} | {properties.version}
+//         </a>
+//         {/* <Greet name="Prajakta" />
+//         <Greet name="Anjali" />
+//         <Greet name="Divya" /> */}
+//         {Hey}
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
